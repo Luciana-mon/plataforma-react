fc5309c40d5d22c45b297193a0f114b800860ceb
Luciana-mon <luciana.monsantos@gmail.com>
2023-03-23 01:57:32 -0300

    criacao de rotas

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,7 @@
+export function Blog() {
+    return (
+        <div className="Blog">
+            <h1>Blog</h1>
+        </div>
+    )
+}
---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,7 @@
+export function Contato() {
+    return (
+        <div className="contato">
+            <h1>contato</h1>
+        </div>
+    )
+}
